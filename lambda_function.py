bcd158161e60d557508b7ec9fbbfc83bdcb4a91a
Zachary Johnson <vepped@gmail.com>
2023-08-24 15:19:52 -0400

    Initial commit

---
+++ lambda_function.py
@@ -0,0 +1,42 @@
+# Use this code snippet in your app.
+# If you need more information about configurations
+# or implementing the sample code, visit the AWS docs:
+# https://aws.amazon.com/developer/language/python/
+
+import boto3
+
+from botocore.exceptions import ClientError
+import requests
+
+
+def get_secret(event,context):
+
+    secret_name = "MentalHealthSlackBotKenzieDisposa"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    print(secret)
+    
+    message = {
+        "text": "What are you thankful for today?"
+    }
+    
+    post = requests.post(secret.MHBWebHook, json=message) 
